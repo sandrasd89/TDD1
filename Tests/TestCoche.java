7466ceb16f0fdfdf899988f2154969c385516fc1
Sandra Sáez Ibarra <sansaeiba@alu.edu.gva.es>
2023-04-02 13:06:07 +0200

    segundo test y método acelerar creados

--- Tests/TestCoche.java
+++ Tests/TestCoche.java
@@ -7,4 +7,12 @@ public class TestCoche {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+
+
 }
